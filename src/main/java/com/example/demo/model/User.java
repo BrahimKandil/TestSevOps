b3db90d24c16d66de2f452fe1ebc5d5ba67e75b3
brahim <ibrahim.kandil@istic.ucar.tn>
2025-05-04 16:53:31 +0200

    adaptation

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class User {
 
+    // Getters and setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,10 +27,7 @@ public class User {
         this.name = name;
     }
 
-    // Getters and setters
-    public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
