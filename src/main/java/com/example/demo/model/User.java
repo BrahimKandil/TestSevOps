98f87d5bedb37b31a2f621141a0b67057d67d8df
brahim <ibrahim.kandil@istic.ucar.tn>
2025-05-04 17:04:33 +0200

    adaptation

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
@@ -35,13 +34,6 @@ public class User {
 
     public void setName(String name) { this.name = name; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
 
 
