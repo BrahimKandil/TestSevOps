edf7e542c348270f5ff0e53b82cb13150d70ce5b
brahim <ibrahim.kandil@istic.ucar.tn>
2025-05-11 23:48:54 +0100

    adaptation

--- src/test/java/com/example/demo/UserServiceIntegrationTest.java
+++ src/test/java/com/example/demo/UserServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import com.example.demo.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
