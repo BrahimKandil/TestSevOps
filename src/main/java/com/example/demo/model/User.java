9a8faaa648c6aa08879e053e744ca5fd56e3049a
brahim <ibrahim.kandil@istic.ucar.tn>
2025-05-12 00:40:59 +0200

    adaptation

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,13 +1,11 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 
 @Entity
+@Table(name = "`users`")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
